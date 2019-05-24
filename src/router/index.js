a43f1878cec6b1bd60754394cbe9b18481a6c6f7
awen <awen@mogujie.com>
2019-05-24 22:28:51 +0800

    ##gg

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import main from '@/components/main'
+import star from '@/components/star_main'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/main',
       name: 'main',
       component: main
+    },
+    {
+      path: '/star',
+      name: 'star',
+      component: star
     }
   ]
 })
